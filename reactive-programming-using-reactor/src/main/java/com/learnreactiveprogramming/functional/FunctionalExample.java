023d3bfa0c183e78f14ff2d89fb56d96a00e3575
ab-zar <105588913+ab-zar@users.noreply.github.com>
2023-01-30 18:25:13 +0000

    Functional vs Imperative filter function
    Simple filter function

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,4 +1,18 @@
 package com.learnreactiveprogramming.functional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionalExample {
+    public static void main(String[] args) {
+        var wordList = List.of("beef", "chicken", "pea", "tea", "buckwheat", "potato", "potato");
+        var filteredWords = wordsLongerThanSize(wordList, 4);
+        System.out.println(filteredWords);
+
+    }
+
+    private static List<String> wordsLongerThanSize(List<String> wordList, int i) {
+        return wordList.stream().filter(word -> word.length() > 3).collect(Collectors.toList());
+    }
 }
--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,4 +1,24 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImperativeExample {
+
+    public static void main(String[] args) {
+        var wordList = List.of("beef", "chicken", "pea", "tea", "buckwheat", "potato", "potato");
+        var filteredWords = wordsLongerThanSize(wordList, 4);
+        System.out.println(filteredWords);
+    }
+
+    private static List<String> wordsLongerThanSize(List<String> wordList, int i) {
+        var filteredWordList = new ArrayList<String>(wordList.size());
+
+        for (String word : wordList) {
+            if (word.length() > 3) {
+                filteredWordList.add(word);
+            }
+        }
+        return filteredWordList;
+    }
 }
