b9aec1811eb4e643a705c696ddb124f1f3cc78eb
ab-zar <105588913+ab-zar@users.noreply.github.com>
2023-01-30 18:36:08 +0000

    Sorting and Parallelism added to Functional style example

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -6,12 +6,12 @@ import java.util.stream.Collectors;
 public class FunctionalExample {
     public static void main(String[] args) {
         var wordList = List.of("beef", "chicken", "pea", "tea", "buckwheat", "potato", "potato");
-        var filteredWords = wordsLongerThanSizeNoDuplicatesToUpperCase(wordList, 3);
+        var filteredWords = wordsLongerThanSizeNoDuplicatesToUpperCaseSortedParallel(wordList, 3);
         System.out.println(filteredWords);
 
     }
 
-    private static List<String> wordsLongerThanSizeNoDuplicatesToUpperCase(List<String> wordList, int i) {
-        return wordList.stream().filter(word -> word.length() > i).map(String::toUpperCase).distinct().collect(Collectors.toList());
+    private static List<String> wordsLongerThanSizeNoDuplicatesToUpperCaseSortedParallel(List<String> wordList, int i) {
+        return wordList.parallelStream().filter(word -> word.length() > i).map(String::toUpperCase).distinct().sorted().collect(Collectors.toList());
     }
 }
