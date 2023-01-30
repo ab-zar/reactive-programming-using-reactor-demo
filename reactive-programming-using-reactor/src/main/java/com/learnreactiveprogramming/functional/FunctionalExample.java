5a64f3e54ff571fb0f797dd2b1175c874045da62
ab-zar <105588913+ab-zar@users.noreply.github.com>
2023-01-30 18:29:53 +0000

    Functional vs Imperative Filter DIstinct
    No duplicates

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,18 +1,17 @@
 package com.learnreactiveprogramming.functional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FunctionalExample {
     public static void main(String[] args) {
         var wordList = List.of("beef", "chicken", "pea", "tea", "buckwheat", "potato", "potato");
-        var filteredWords = wordsLongerThanSize(wordList, 4);
+        var filteredWords = wordsLongerThanSizeNoDuplicates(wordList, 4);
         System.out.println(filteredWords);
 
     }
 
-    private static List<String> wordsLongerThanSize(List<String> wordList, int i) {
-        return wordList.stream().filter(word -> word.length() > 3).collect(Collectors.toList());
+    private static List<String> wordsLongerThanSizeNoDuplicates(List<String> wordList, int i) {
+        return wordList.stream().filter(word -> word.length() > i).distinct().collect(Collectors.toList());
     }
 }
--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -7,15 +7,15 @@ public class ImperativeExample {
 
     public static void main(String[] args) {
         var wordList = List.of("beef", "chicken", "pea", "tea", "buckwheat", "potato", "potato");
-        var filteredWords = wordsLongerThanSize(wordList, 4);
+        var filteredWords = wordsLongerThanSizeNoDuplicates(wordList, 4);
         System.out.println(filteredWords);
     }
 
-    private static List<String> wordsLongerThanSize(List<String> wordList, int i) {
+    private static List<String> wordsLongerThanSizeNoDuplicates(List<String> wordList, int i) {
         var filteredWordList = new ArrayList<String>(wordList.size());
 
         for (String word : wordList) {
-            if (word.length() > 3) {
+            if (word.length() > i && !filteredWordList.contains(word)) {
                 filteredWordList.add(word);
             }
         }
